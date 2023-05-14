c45c09196982bff77102e324620d1bb5e441cac6
Invokerhuu <734004243@qq.com>
2023-05-14 05:14:14 -0400

    add config file

---
+++ init.lua
@@ -0,0 +1,30 @@
+require("a")
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	pattern = { "*" },
+	callback = function()
+		vim.highlight.on_yank({
+			timeout = 300,
+		})
+	end,
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
